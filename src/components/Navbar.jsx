bd2bee4efa06a532d888f54e64d228332e22bfd8
danieltgilbert <daniel.g@tltechnologies.net>
2023-03-22 18:09:05 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Stack } from "@mui/material";
+import { Link } from 'react-router-dom';
+
+import { logo } from '../utils/constants';
+
+const Navbar = () => (
+  <Stack>
+    
+  </Stack>
+)
+
+export default Navbar
